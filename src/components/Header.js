da14e347c9c1bcb3ff55f48c593e46e90c539400
hotvely <hwan_o3o@naver.com>
2023-09-18 16:50:19 +0900

    feat : react 파일 기본 설정 및 구조.

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1>헤더임미다;</h1>;
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hom..e.. 이에여</h1>
+      <Outlet />
+    </>
+  );
+};
+
+export default Home;
