ef550b6c22a23fad1d6cf34a287f670835ecda2e
hotvely <hwan_o3o@naver.com>
2023-09-19 17:13:39 +0900

    feat : 2023_09_19 게시판 화면 구성 fixed 분리

--- src/components/LSide.js
+++ src/components/LSide.js
@@ -1,16 +1,42 @@
 import styled from "styled-components";
 
 const StyledLSide = styled.div`
-  background-color: red;
+  background-color: pink;
   width: 15%;
-  justify-content: left;
+  height: 80%;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  .lMenu {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    margin: 50px 0;
+    border: 1px solid black;
+    justify-content: center;
+    div {
+      padding: 20px 0;
+    }
+
+    text-align: center;
+  }
 `;
 
 const LSide = () => {
   return (
     <>
       <StyledLSide>
-        <div>왼쪽...</div>
+        <div className="lMenu">
+          <div>왼쪽메뉴..</div>
+          <div>게시판</div>
+          <div>게시판</div>
+          <div>게시판</div>
+          <div>게시판</div>
+          <div>게시판</div>
+        </div>
       </StyledLSide>
     </>
   );
--- src/components/RSide.js
+++ src/components/RSide.js
@@ -1,15 +1,40 @@
 import styled from "styled-components";
 
 const StyledRSide = styled.div`
-  background-color: black;
+  background-color: pink;
   width: 15%;
+  height: 80%;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+
+  .rMenu {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    margin: 50px 0;
+
+    justify-content: center;
+    div {
+      border: 1px solid black;
+      margin: 20px 0;
+    }
+
+    text-align: center;
+  }
 `;
 
 const RSide = () => {
   return (
     <>
       <StyledRSide>
-        <div>오른쪽 사이드 입니당..</div>
+        <div className="rMenu">
+          <div>우측 기능1</div>
+          <div>우측 기능1</div>
+          <div>우측 기능1</div>
+        </div>
       </StyledRSide>
     </>
   );
