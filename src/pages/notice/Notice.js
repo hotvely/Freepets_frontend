f94699f2cc2f96527fb3f8d35335fb73dee50e5d
hotvely <hwan_o3o@naver.com>
2023-11-03 13:03:47 +0900

    feat : 2023_11_03 css 공지사항... ㅜㅜ

--- src/pages/notice/Notice.js
+++ src/pages/notice/Notice.js
@@ -24,7 +24,7 @@ import { getTokenCookie } from "../../api/cookie";
 import { userLogout } from "../../components/store/userSlice";
 
 const MainStlye = styled.div`
-  flex-basis: 50%;
+  flex-basis: 880px;
   flex-shrink: 0;
   flex-grow: 1;
 `;
@@ -32,9 +32,9 @@ const MainStlye = styled.div`
 const MainBanner = styled.div`
   display: flex;
   align-items: center;
-  width: 100%;
   padding-bottom: 0 30px;
-  flex-basis: 800px;
+  flex-basis: 600px;
+  flex-shrink: 0;
   .banner-img {
     width: 100%;
     img {
@@ -45,8 +45,10 @@ const MainBanner = styled.div`
 
 const MainContentBox = styled.div`
   border: 1px solid #3a98b9;
-  flex-basis: 800px;
+  flex-basis: 600px;
   margin-top: 20px;
+  flex-shrink: 0;
+  flex-grow: 1;
   /* display: flex;
   flex-direction: column; */
 
@@ -136,7 +138,7 @@ const MainContentBox = styled.div`
     /* justify-content: space-between; */
     /* flex-wrap: wrap; */
     margin: 10px;
-    width: 100%-10px;
+
     gap: 10px;
     /* padding: 10px; */
     /* margin-right: 10px; */
